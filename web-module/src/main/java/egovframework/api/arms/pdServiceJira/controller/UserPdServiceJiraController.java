980ca57abe5463ec6e1296febf2c4045fc2e2fec
LeeDongMin <admin@313.co.kr>
2022-01-09 21:20:07 +0900

    [SmartCommit] SP-313 #comment 제품(서비스) Jira 연동 API #close #time 1h +review SR @dongmin.lee
    [ Java Service Tree Framework(JSTF)::Backend-Server ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- web-module/src/main/java/egovframework/api/arms/pdServiceJira/controller/UserPdServiceJiraController.java
+++ web-module/src/main/java/egovframework/api/arms/pdServiceJira/controller/UserPdServiceJiraController.java
@@ -1,5 +1,6 @@
 package egovframework.api.arms.pdServiceJira.controller;
 
+import egovframework.api.arms.pdService.model.PdServiceDTO;
 import egovframework.api.arms.pdServiceJira.model.PdServiceJiraDTO;
 import egovframework.api.arms.pdServiceJira.service.PdServiceJira;
 import egovframework.com.ext.jstree.springHibernate.core.controller.SHVAbstractController;
@@ -8,9 +9,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.PostConstruct;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping(value = {"/auth-user/api/arms/pdServiceJira"})
@@ -26,4 +34,19 @@ public class UserPdServiceJiraController extends SHVAbstractController<PdService
     }
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ResponseBody
+    @RequestMapping(value = "/getTestConnectionJira.do", method = RequestMethod.POST)
+    public ModelAndView addEndNodeByRoot(PdServiceJiraDTO pdServiceJiraDTO,
+                                         BindingResult bindingResult) throws Exception {
+        if (bindingResult.hasErrors())
+            throw new RuntimeException();
+
+
+
+        ModelAndView modelAndView = new ModelAndView("jsonView");
+        modelAndView.addObject("result", "");
+
+        return modelAndView;
+    }
 }
